bd9a2aacd9c36a25213a3c45ed21a42e71f17539
Achu-24 <hamshaverthinik@gmail.com>
2025-03-26 10:52:44 +0530

    Update ServiceCard.jsx

--- fullstackca1/src/components/ServiceCard.jsx
+++ fullstackca1/src/components/ServiceCard.jsx
@@ -2,11 +2,11 @@
 import React from 'react'
 import './ServiceCard.css'
 
-function ServiceCard(Servicetitle,Servicedescription) {
+function ServiceCard({Servicetitle,Servicedescription}) {
   return (
     <div className='Service-Card'>
-        <h2>Servicetitle</h2>
-        <p>Servicedescription</p>
+        <h2>{Servicetitle}</h2>
+        <p>{Servicedescription}</p>
     </div>
   )
 }
