dbb0bcfc56314bbcd69813fcb508099263013928
Hamshaverthini K <hamshaverthinik@gmail.com>
2025-03-26 10:05:28 +0530

    updated

--- fullstackca1/src/components/ServiceCard.jsx
+++ fullstackca1/src/components/ServiceCard.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './ServiceCard.css'
 
 function ServiceCard(Servicetitle,Servicedescription) {
   return (
-    <div className='ServiceCard'>
+    <div className='Service-Card'>
         <h2>{Servicetitle}</h2>
         <p>{Servicedescription}</p>
     </div>
